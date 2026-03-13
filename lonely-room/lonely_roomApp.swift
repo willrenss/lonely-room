1045f975cc26cf91b7c7927a728b089c95343a77
William Lourensius <wlourensius@gmail.com>
2026-03-13 16:02:26 +0700

    Initial Commit

---
+++ lonely-room/lonely_roomApp.swift
@@ -0,0 +1,17 @@
+//
+//  lonely_roomApp.swift
+//  lonely-room
+//
+//  Created by William Lourensius on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct lonely_roomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
